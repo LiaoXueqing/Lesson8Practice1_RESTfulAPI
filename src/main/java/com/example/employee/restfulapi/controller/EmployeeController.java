c2d534a6bcffe17d660112bbc55557d649da6293
xuexue <1047504737@qq.com>
2018-05-14 21:12:34 +0800

    update code

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -17,36 +17,36 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     //1.获取employee列表
-    @GetMapping(value = "")
+    @GetMapping("")
     public ResponseEntity getEmployees() throws Exception {
         return new ResponseEntity<>(employeeRepository.findAll(), HttpStatus.OK);
     }
     //2.获取某个具体employee
-    @GetMapping(value="/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity getEmployeeById(@PathVariable Long id) throws Exception {
         return new ResponseEntity<>(employeeRepository.findOne(id), HttpStatus.OK);
     }
     //3.分页查询，page等于1，pageSize等于5
-    @GetMapping(value="page/{page}/pageSize/{pageSize}")
+    @GetMapping("page/{page}/pageSize/{pageSize}")
     public ResponseEntity getEmployeeByPage(@PathVariable int page, @PathVariable int pageSize) throws Exception {
         Pageable pageable = new PageRequest(page - 1, pageSize);
         return new ResponseEntity<>(employeeRepository.findAll(pageable), HttpStatus.OK);
     }
 
     //4.筛选出所有男性Employee
-    @GetMapping(value="/male")
+    @GetMapping("/male")
     public ResponseEntity getMaleEmployees() throws Exception{
         return new ResponseEntity<>(employeeRepository.findByGender("male"), HttpStatus.OK);
     }
 
     //5.增加一个employee
-    @PostMapping(value="")
+    @PostMapping("")
     public ResponseEntity addEmployee(@RequestBody Employee employee) throws Exception{
         employeeRepository.save(employee);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
     //6.更新某个employee
-    @PutMapping(value="/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity updateEmployeeById(@PathVariable Long id, @RequestBody Employee newEmployee) throws Exception{
         Employee employee = employeeRepository.findOne(id);
         employee.setAge(newEmployee.getAge());
@@ -58,7 +58,7 @@ public class EmployeeController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     //7.删除某个employee
-    @DeleteMapping(value="/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity deleteEmployeeById(@PathVariable long id) throws Exception{
         employeeRepository.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
